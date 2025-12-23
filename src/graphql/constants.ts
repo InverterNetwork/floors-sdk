623f49f816977394217bfb63f0686ad1d93a0364
Dou <hello@dogu.me>
2025-12-23 18:44:46 +0300

    fix(graphql): update GraphQL endpoint to latest version in multiple files

--- src/graphql/constants.ts
+++ src/graphql/constants.ts
@@ -1,1 +1,1 @@
-export const DEFAULT_GRAPHQL_URL = 'https://indexer.dev.hyperindex.xyz/ffec162/v1/graphql'
+export const DEFAULT_GRAPHQL_URL = 'https://indexer.dev.hyperindex.xyz/91ca2ea/v1/graphql'
