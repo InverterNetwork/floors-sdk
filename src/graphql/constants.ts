8b7977eb1477435affd2234d120b9c5042345653
0xxmemo <mguleryuz3@gmail.com>
2026-02-17 19:12:59 -0500

    fix(graphql): update default GraphQL URL to new endpoint

--- src/graphql/constants.ts
+++ src/graphql/constants.ts
@@ -1,2 +1,2 @@
 export const DEFAULT_GRAPHQL_URL =
-  process.env.NEXT_PUBLIC_GRAPHQL_URL || 'https://indexer.hyperindex.xyz/84826af/v1/graphql'
+  process.env.NEXT_PUBLIC_GRAPHQL_URL || 'https://indexer.hyperindex.xyz/cada10d/v1/graphql'
